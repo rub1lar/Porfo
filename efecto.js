ecc4886608eeb60186059f1a23eb1ccb8ce4bc49
seba5 <sebastianrubilar27@gmail.com>
2023-04-15 15:00:03 -0300

    arreglos

--- efecto.js
+++ efecto.js
@@ -12,7 +12,7 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3000);}
+  setTimeout(carousel, 6000);}
   
   
   var slideIndex = 1;
