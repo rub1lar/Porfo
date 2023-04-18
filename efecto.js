51f9b45c0cdd8027b1d6d50560c1391454175f4c
seba5 <sebastianrubilar27@gmail.com>
2023-04-18 13:00:14 -0300

    arreglo de cierre de boton hamburguesa

--- efecto.js
+++ efecto.js
@@ -32,3 +32,62 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+
+
+//////////funcion casera para el boton hamburguesa////
+var boton = document.getElementById("nav-btn")
+var botonHamburguesa = document.getElementById("nav-links");
+var estado=0;
+botonHamburguesa.addEventListener("click", function() {
+  botonHamburguesa.style.height ="0px";
+  if (estado =1){
+    boton.addEventListener("click", function() {
+      botonHamburguesa.style.height ="100vh";
+    });}
+    else {
+      boton.addEventListener("click", function() {
+        botonHamburguesa.style.height ="0px";
+      });
+    
+        estado =1; 
+    }
+  
+  
+
+});
+
+
+
+ 
+
+
+
+/* var boton = document.getElementById("nav-btn");
+
+botonHamburguesa.addEventListener("click", function() {
+  botonHamburguesa.style.display ="";
+});
+ */
+
+
+
+
+
+/* for (var i = 0; i < hamburguerButton.length; i++) {
+  hamburguerButton[i].style.display = "none";
+} */
+
+// Agregue un controlador de eventos de clic a cada enlace del menú
+/* menuLinks.forEach((link) => {
+  link.addEventListener('click', () => { */
+    // Remueva la clase CSS que muestra el botón hamburguesa
+/*     for (var i = 0; i < hamburguerButton.length; i++) {
+      hamburguerButton[i].style.display = "none";
+      menuLinks.style.height ="0px";
+    }
+  });
+});
+ */
+
+
